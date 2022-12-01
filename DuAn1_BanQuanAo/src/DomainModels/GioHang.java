584c1afc349d0c16ed1be5fb017e5614ebe528b9
Hacv28730 <118011867+hikarusinju@users.noreply.github.com>
2022-12-01 10:55:16 +0700

    GioHang

---
+++ DuAn1_BanQuanAo/src/DomainModels/GioHang.java
@@ -0,0 +1,114 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DomainModels;
+
+/**
+ *
+ * @author DELL
+ */
+public class GioHang {
+    String maHD;
+    String maSP;
+    String tenSP;
+    int donGia;
+    int soLuong;
+    int tongTien;
+    int tienKhachDua;
+    int tienThua;
+    String trangThai;
+
+    public GioHang() {
+    }
+
+    public GioHang(String maHD, String maSP, String tenSP, int donGia, int soLuong, int tongTien, int tienKhachDua, int tienThua, String trangThai) {
+        this.maHD = maHD;
+        this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.donGia = donGia;
+        this.soLuong = soLuong;
+        this.tongTien = tongTien;
+        this.tienKhachDua = tienKhachDua;
+        this.tienThua = tienThua;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public int getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(int donGia) {
+        this.donGia = donGia;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(int tongTien) {
+        this.tongTien = tongTien;
+    }
+
+    public int getTienKhachDua() {
+        return tienKhachDua;
+    }
+
+    public void setTienKhachDua(int tienKhachDua) {
+        this.tienKhachDua = tienKhachDua;
+    }
+
+    public int getTienThua() {
+        return tienThua;
+    }
+
+    public void setTienThua(int tienThua) {
+        this.tienThua = tienThua;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public int tinhTong(){
+        setTongTien(soLuong*donGia);
+        return tongTien;
+    }
+    
+}
